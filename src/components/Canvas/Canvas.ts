8de0df22bfc84d6ff77cf9380377678201e1ca37
Aliaksei Yakubuk <aliaksei.yakubuk@gmail.com>
2023-11-20 03:01:58 +0100

    start skills page

--- src/components/Canvas/Canvas.ts
+++ src/components/Canvas/Canvas.ts
@@ -4,15 +4,31 @@
 import { Point } from '../Point';
 import { Rectangle } from '../Rectangle';
 
+type CursorPrintOptions = {
+  start?: {
+    ttl?: number,
+  },
+  end?: {
+    ttl?: number,
+    sync?: boolean
+  },
+  period?: number
+};
+
+type SpeedPrintOptions = {
+  min?: number,
+  max?: number
+};
+
 type PrintOptions = {
   fontSize?: number,
   fontFamily?: string,
   align?: CanvasTextAlign,
   color?: string,
   background?: string,
-  minDelay?: number,
-  maxDelay?: number,
-  animate?: boolean
+  animate?: boolean,
+  speed?: SpeedPrintOptions,
+  cursor?: CursorPrintOptions
 };
 
 class Canvas {
@@ -86,12 +102,26 @@ class Canvas {
     return `${this.getFontSize(options)}px ${this.getFontFamily(options)}`;
   }
 
-  private getMinDelay(options?: PrintOptions) {
-    return options?.minDelay || 0;
+  private getSpeed(options?: PrintOptions) {
+    const speed = options?.speed;
+    return {
+      min: speed?.min || 100,
+      max: speed?.max || 100,
+    };
   }
 
-  private getMaxDelay(options?: PrintOptions) {
-    return options?.maxDelay || 0;
+  private getCursor(options?: PrintOptions) {
+    const cursor = options?.cursor;
+    return {
+      period: cursor?.period || 400,
+      start: {
+        ttl: cursor?.start?.ttl || 0,
+      },
+      end: {
+        ttl: cursor?.end?.ttl || 0,
+        sync: cursor?.end?.sync !== false,
+      },
+    };
   }
 
   private getRectangleForText(textPosition: Point, options?: PrintOptions) {
@@ -107,11 +137,12 @@ class Canvas {
     context.textAlign = 'left';
     context.font = this.getFont(options);
 
+    const metadata = context.measureText(input);
     const width = input
       .split('')
       .reduce((total, char) => (total + context.measureText(char).width), 0);
 
-    return { width };
+    return { ...metadata, width };
   }
 
   private drawRect(rectangle: Rectangle, options?: PrintOptions) {
@@ -127,18 +158,14 @@ class Canvas {
   private drawCursor(posx: Point, options?: PrintOptions) {
     const height = this.getFontSize(options);
     const topLeft = new Point(posx.x, posx.y - height / 2);
-    const bottomRight = new Point(posx.x + height / 2, posx.y + height / 2);
+    const bottomRight = new Point(posx.x + height / 2.5, posx.y + height / 2.5);
     const rectangle = new Rectangle(topLeft, bottomRight);
 
     this.drawRect(rectangle, { ...options, background: this.getTextColor(options) });
   }
 
   private removeCursor(posx: Point, options?: PrintOptions) {
-    // const prevGlobalCompositeOperation = this.context.globalCompositeOperation;
-    // this.context.globalCompositeOperation = 'destination-over';
-    // debugger;
     this.drawCursor(posx, { ...options, color: this.getBackground(options) });
-    // this.context.globalCompositeOperation = prevGlobalCompositeOperation;
   }
 
   private drawText(input: string, origin: Point, options?: PrintOptions) {
@@ -156,37 +183,67 @@ class Canvas {
     this.drawText(input, new Point(positionX, positionY), options);
   }
 
+  private async drawAnimatedCursor(position: Point, ttl: number, options?: PrintOptions) {
+    const startTime = Date.now();
+    const cursor = this.getCursor(options);
+
+    if (ttl < (1 * cursor.period)) {
+      return;
+    }
+
+    const animateCursor = async () => {
+      const endTime = Date.now();
+      const diff = endTime - startTime;
+
+      this.drawCursor(position, options);
+      await this.waitFor(cursor.period);
+      requestAnimationFrame(() => { this.removeCursor(position, options); });
+      await this.waitFor(cursor.period);
+
+      if (diff < ttl) {
+        await animateCursor();
+      }
+    };
+
+    if (cursor.end.sync) {
+      await animateCursor();
+    } else {
+      animateCursor();
+    }
+  }
+
   // eslint-disable-next-line max-len
-  private async drawTextLineProgressively(input: string, rectangle: Rectangle, options?: PrintOptions) {
-    const minDelay = this.getMinDelay(options);
-    const maxDelay = this.getMaxDelay(options);
+  private async drawAnimatedTextLine(input: string, rectangle: Rectangle, options?: PrintOptions) {
+    const speed = this.getSpeed(options);
+    const cursor = this.getCursor(options);
     const metadata = this.measureText(input, options);
     const positionX = (rectangle.width / 2) - (metadata.width / 2);
     const positionY = rectangle.topLeft.y + rectangle.height / 2;
-
-    this.context.textAlign = 'left';
-    this.context.textBaseline = 'middle';
-    this.context.fillStyle = this.getTextColor(options);
-    this.context.font = this.getFont(options);
+    const textOptions = { ...options, align: 'left' } as PrintOptions;
 
     let index = 0;
     let position = positionX;
+
+    await this.drawAnimatedCursor(new Point(positionX, positionY), cursor.start.ttl, options);
 
     for (index; index < input.length; index += 1) {
       const char = input[index];
 
       requestAnimationFrame(() => {
         const { width } = this.measureText(char, options);
+        const nextPosition = position + width;
 
-        this.drawText(char, new Point(position, positionY), { ...options, align: 'left' });
-        this.drawCursor(new Point(position + width, positionY), options);
+        this.drawText(char, new Point(position, positionY), textOptions);
+        this.drawCursor(new Point(nextPosition, positionY), options);
 
-        position += width;
+        position = nextPosition;
       });
-      await this.waitFor(this.randomNumberInRange(minDelay, maxDelay));
+      await this.waitFor(this.randomNumberInRange(speed.min, speed.max));
 
       this.removeCursor(new Point(position, positionY), options);
     }
+
+    await this.drawAnimatedCursor(new Point(position, positionY), cursor.end.ttl, options);
   }
 
   constructor(htmlCanvas: HTMLCanvasElement | null) {
@@ -200,12 +257,34 @@ class Canvas {
 
   public async print(input: string, position: Point, options?: PrintOptions) {
     const rectangle = this.getRectangleForText(position, options);
-    // this.drawRect(rectangle, options);
 
     if (options?.animate) {
-      await this.drawTextLineProgressively(input, rectangle, options);
+      await this.drawAnimatedTextLine(input, rectangle, options);
     } else {
       this.drawTextLine(input, rectangle, options);
+    }
+  }
+
+  public getPixelData(point: Point) {
+    const imageData = this.context.getImageData(point.x, point.y, this.context.canvas.width, 1);
+    //     const arr = new Uint8ClampedArray(4);
+    //     arr[0] = 20;
+    // = arr;    arr[1] = 40;
+    //     arr[2] = 60;
+    //     console.log(data);
+    //     const imageData = new ImageData(4, 1);
+    //     imageData.data
+
+    for (let index = 0; index < imageData.data.length; index += 1) {
+      // if (data[index] !== 0) {
+      console.log(imageData.data[index]);
+      // }
+
+      if (index > 1000) {
+        imageData.data[index] = 255;
+      }
+      console.log(imageData);
+      this.context.putImageData(imageData, point.x, point.y);
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,29 @@ import { Canvas } from './components/Canvas';
 import { router } from './components/Router';
 import { Point } from './components/Point';
 
-const navigation = new Navigation();
+// const navigation = new Navigation();
 const logo = await Canvas.init('logo');
 
 router.listenLocationUpdates();
 router.listenPopStateUpdates();
-navigation.listenWheelUpdates();
+// navigation.listenWheelUpdates();
 
 await logo.print('Aliaksei Yakubuk', new Point(0, 90), {
-  fontSize: 35,
+  fontSize: 55,
+  fontFamily: 'ubuntu',
   color: '#fff',
   background: '#000',
   animate: true,
-  maxDelay: 50,
-  minDelay: 50,
+  speed: { min: 50, max: 50 },
+  cursor: { start: { ttl: 700 } },
 });
 
 await logo.print('Software Engineer', new Point(0, 150), {
-  fontSize: 20,
+  fontSize: 35,
+  fontFamily: 'ubuntu',
   color: '#fff',
   background: '#000',
   animate: true,
-  maxDelay: 50,
-  minDelay: 50,
+  speed: { min: 50, max: 50 },
+  cursor: { end: { ttl: Infinity, sync: false } },
 });
