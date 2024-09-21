6156df594ade7eb90232c0c46562887593e2051a
Aliaksei Yakubuk <aliaksei.yakubuk@gmail.com>
2024-09-21 13:22:16 +0200

    revert removal redirects from important domains
    This reverts commit b463c7b093225aefdb6fcfe32310f9df35e6b979.

--- scripts/generate-sitemap.sh
+++ scripts/generate-sitemap.sh
@@ -12,7 +12,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>' >> $file;
 echo '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $file;
 
 for url in $urls; do 
-  echo "\n<url>\n<loc>https://$url/</loc>\n<lastmod>$timestamp</lastmod>\n</url>" >> $file;
+  echo "\n<url>\n<loc>$url</loc>\n<lastmod>$timestamp</lastmod>\n</url>" >> $file;
 done
 
 echo '\n</urlset>' >> $file;
